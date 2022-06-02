bf98b11da944b96925229b6fbdc0530366546e51
Elioth Coder <elioth.coder@gmail.com>
2022-06-03 03:11:26 +0800

    Removed creation and deployment of uploads folder.

--- ready.bat
+++ ready.bat
@@ -2,8 +2,8 @@ git clone https://github.com/elioth-coder/generaltinio.gov.ph.git
 cd generaltinio.gov.ph
 del .gitignore
 rmdir /s /q .git
-if not exist uploads mkdir uploads
-type nul > uploads\filename.txt
+@REM if not exist uploads mkdir uploads
+@REM type nul > uploads\filename.txt
 call composer install
 call composer dump-autoload -o
 del papaya.sql
